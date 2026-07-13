d5ff2b64158b8ed9b3b8af7628b09b6896027368
Manuel A. Pianini <manuelantoniopianini@gmail.com>
2026-07-13 16:11:39 +0200

    Added tables and refactored queries to work both with no filter and with specific submolts

--- edge_list.py
+++ edge_list.py
@@ -12,33 +12,25 @@ pd.set_option('display.max_columns', None)
 con = duckdb.connect()
 
 # Create tables named comments and posts
-Tables = 1
-if Tables:
-    con.execute("""
-    CREATE TABLE comments AS
-    SELECT id, agent_id, post_id, parent_id
-    FROM read_csv_auto('full_comments.csv', nullstr=['NaT', 'NA', 'N/A', 'null'])
-    """)
+con.execute("""
+CREATE TABLE comments AS
+SELECT id, agent_id, post_id, parent_id
+FROM read_csv_auto('full_comments.csv', nullstr=['NaT', 'NA', 'N/A', 'null'])
+""")
 
-    con.execute("""
-    CREATE TABLE comments2 AS
-    SELECT id, agent_id, parent_id
-    FROM read_csv_auto('full_comments.csv', nullstr=['NaT', 'NA', 'N/A', 'null'])
-    """)
+con.execute("""
+CREATE TABLE posts AS
+SELECT submolt, agent_id, id
+FROM read_csv_auto('full_posts.csv', nullstr=['NaT', 'NA', 'N/A', 'null'])
+""")
 
-    con.execute("""
-    CREATE TABLE posts AS
-    SELECT submolt, agent_id, id
-    FROM read_csv_auto('full_posts.csv', nullstr=['NaT', 'NA', 'N/A', 'null'])
-    """)
+con.execute("""
+CREATE TABLE agents AS
+SELECT id, name
+FROM read_csv_auto('full_agents.csv', nullstr=['NaT', 'NA', 'N/A', 'null'])
+""")
 
-    con.execute("""
-    CREATE TABLE agents AS
-    SELECT id, name
-    FROM read_csv_auto('full_agents.csv', nullstr=['NaT', 'NA', 'N/A', 'null'])
-    """)
-
-# Query: users that posts
+# Query: users that posted or commented
 poster_users = con.execute("""
 -- Users who commented or posted
 SELECT DISTINCT
@@ -63,19 +55,43 @@ ORDER BY name
 poster_users.to_csv("poster_users.csv")
 
 
-# Query: linked users
-con.execute("""
-CREATE TABLE linked AS
+# ========================================================
+# Queries and tables for submolts filtered comments & users
+
+SUBMOLT = 'philosophy'
+PREFIX = SUBMOLT + "_" if SUBMOLT != '' else ''
+
+SUBMOLT_COMMENTS = PREFIX + "comments"
+LINKED_TABLE = PREFIX + "linked"
+
+if SUBMOLT != '':
+    # Table for filtered comments by submolt
+    con.execute(f"""
+    CREATE TABLE '{SUBMOLT_COMMENTS}' AS
+    SELECT 
+        c.id AS id,
+        c.agent_id AS agent_id,
+        c.post_id AS post_id,
+        c.parent_id AS parent_id,
+        submolt
+    FROM comments c
+    JOIN (SELECT * FROM posts WHERE submolt = '{SUBMOLT}') p
+        ON c.post_id = p.id
+    """)
+
+# Query: users that are linked via commenting, optionally filtered by submolts
+con.execute(f"""
+CREATE TABLE '{LINKED_TABLE}' AS
 SELECT DISTINCT  --DISTINCT should be redundant
 LEAST (cid_1, cid_2) AS agent1,
 GREATEST (cid_1, cid_2) AS agent2
 FROM (
 
-    -- Users who commented to a posts
+    -- Users who commented to a post in SUBMOLT
     SELECT
         c.agent_id AS cid_1,
-        p.agent_id AS cid_2
-    FROM comments c
+        p.agent_id AS cid_2,
+    FROM '{SUBMOLT_COMMENTS}' c
     JOIN posts p
         ON c.post_id = p.id
 
@@ -85,36 +101,38 @@ FROM (
     SELECT
         c.agent_id AS cid_1,
         c2.agent_id AS cid_2
-    FROM comments c
-    JOIN (SELECT * FROM comments2 WHERE parent_id != '0') c2
+    FROM '{SUBMOLT_COMMENTS}' c
+    JOIN (SELECT * FROM '{SUBMOLT_COMMENTS}' WHERE parent_id <> '0') c2
         ON c.id = c2.parent_id
 
-) linked
+)
 WHERE agent1 <> agent2
 ORDER BY agent1, agent2
 """)
 
-con.execute("""
-COPY linked
-TO 'linked_users.csv'
+con.execute(f"""
+COPY '{LINKED_TABLE}'
+TO '{LINKED_TABLE}_users.csv'
 (FORMAT CSV, HEADER);
 """)
 
-active_users = con.execute("""
+# Users list who commented or have been commented onto
+active_users = con.execute(f"""
 SELECT a.id AS id, name
 FROM (
     SELECT agent1 AS id
-    FROM linked
+    FROM {LINKED_TABLE}
                             
     UNION
 
     SELECT agent2 AS id
-    FROM linked
+    FROM {LINKED_TABLE}
     ) a
     JOIN agents 
         ON a.id = agents.id
+ORDER BY name
 """).fetchdf()
-active_users.to_csv("active_users.csv")
+active_users.to_csv(f"{PREFIX}active_users.csv")
 
 
 # Equivalent query
